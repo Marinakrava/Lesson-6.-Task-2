92a85bbfdd9d6977f033935d833ac4ae31d96f5a
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-10-17 22:42:05 +0300

    Добавьте файлы проекта.

---
+++ Lesson 6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int count = str.Length;
+            string str2 = "";
+            foreach (char c in str)
+            {
+                str2 = c + str2;
+            }
+            Console.WriteLine(str2);
+            if (str == str2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+           
+            Console.ReadKey();
+        }
+    }
+}
